53f1717223c4fd6f4206dec45466d030c25fb1ad
harshit <upadhyayharshit05@gmail.com>
2023-03-12 08:55:47 +0530

    Second Commit

--- src/components/Header_Assets/Calendar.jsx
+++ src/components/Header_Assets/Calendar.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import {TbArrowsUpDown} from 'react-icons/tb'
 import '/home/harshit/Desktop/React/attendence-management/src/style/Calendar.css';
 const Calendar=()=>{
+    const [showYear,setShowYear]=useState(false);
+    const handleClickbtn=()=>{
+        console.log('happy')
+        setShowYear(!showYear);
+    }
     return(
         <>
         <div className="Container">
@@ -8,7 +14,28 @@ const Calendar=()=>{
         <label id="Text-Heading" htmlFor="SelectYear">
             Select Year for Viewing the Attendance 
         </label><br />
-        <input type="date" />
+        <div className="selection-box">
+        <button onClick={handleClickbtn} id="select-year" >  Select Year  <TbArrowsUpDown className="inside-btn-icon"/>
+        </button>
+
+        {/* show element on click  */}
+        {
+            showYear &&(
+                <div className="year-chart">
+                    <div>
+                        
+                <button>YEAR 2022</button>
+                <button>YEAR 2023</button>
+                <button>YEAR 2024</button>
+                <button>YEAR 2025</button>
+                <button>YEAR 2026</button>
+                    </div>
+                </div>
+            )
+        }
+       
+        </div>
+       
        </form>
      
         </div>
